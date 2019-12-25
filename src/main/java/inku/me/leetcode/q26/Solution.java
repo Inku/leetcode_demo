5fcd4b007661ccb38d6a5502f3fa6fb7b77adbfc
Inku <inku@inku.me>
2019-12-25 15:39:25 +0800

    提交Q26

--- src/main/java/inku/me/leetcode/q26/Solution.java
+++ src/main/java/inku/me/leetcode/q26/Solution.java
@@ -32,17 +32,4 @@ class Solution extends TestSupport {
         }
         return length;
     }
-
-    //官方解答
-    private int removeDuplicates1(int[] nums) {
-        if (nums.length == 0) return 0;
-        int i = 0;
-        for (int j = 1; j < nums.length; j++) {
-            if (nums[j] != nums[i]) {
-                i++;
-                nums[i] = nums[j];
-            }
-        }
-        return i + 1;
-    }
 }
